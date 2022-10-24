1e772d6ba3621486b40a955a30bcf0e7ce36acaf
ckyfistcom <93664767+mckydot@users.noreply.github.com>
2022-10-25 03:29:53 +0900

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,23 @@
+function member(click){
+    window.location.href = '../member.html';
+}
+
+const left = document.querySelector(".left");
+const right = document.querySelector(".right");
+const container = document.querySelector(".container");
+
+left.addEventListener("mouseenter", () => {
+  container.classList.add("hover-left");
+});
+
+left.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-left");
+});
+
+right.addEventListener("mouseenter", () => {
+  container.classList.add("hover-right");
+});
+
+right.addEventListener("mouseleave", () => {
+  container.classList.remove("hover-right");
+});
